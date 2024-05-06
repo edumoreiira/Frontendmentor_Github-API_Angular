47ca1246a079e4c3b422e96e112dc264e3ef311d
Eduardo Moreira <dudu_moreira9@hotmail.com>
2024-05-06 13:51:53 -0300

    added theme-colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GithubSearchComponent } from './components/github-search/github-search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GithubSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/github-search/github-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './github-search.component.html',
+  styleUrl: './github-search.component.scss'
+})
+export class GithubSearchComponent {
+
+}
