1422e2caf8b66f46a15b7b91223808e06e98385e
Eduardo Moreira <dudu_moreira9@hotmail.com>
2024-05-09 16:05:04 -0300

    added dark theme function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GithubSearchComponent } from './components/github-search/github-search.component';
+import { NgClass } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { GitHubSearchService } from './services/git-hub-search.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GithubSearchComponent],
+  imports: [RouterOutlet, GithubSearchComponent, NgClass, HttpClientModule],
+  providers:[GithubSearchComponent, GitHubSearchService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Frontendmentor_Github-API_Angular';
+
+  theme: boolean = true; 
+  toggleDarkMode(value: boolean){
+    this.theme = value;
+  }
+  ok(){
+    console.log(this.theme)
+  }
 }
--- src/app/components/github-search/github-search.component.ts
+++ src/app/components/github-search/github-search.component.ts
@@ -1,33 +1,87 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GitHubSearchService } from '../../services/git-hub-search.service';
 import { GitHubSearch } from '../../models/search.model';
-import { Observable } from 'rxjs';
+import { Observable, share, shareReplay, startWith, take } from 'rxjs';
 import { FormsModule } from '@angular/forms';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-github-search',
   standalone: true,
-  imports: [HttpClientModule, FormsModule, AsyncPipe],
+  imports: [HttpClientModule, FormsModule, AsyncPipe, CommonModule],
   providers:[GitHubSearchService],
   templateUrl: './github-search.component.html',
   styleUrl: './github-search.component.scss'
 })
-export class GithubSearchComponent{
+export class GithubSearchComponent implements OnInit{
+
   
   constructor(private githubService: GitHubSearchService){
   }
   //search response
   searchResponse = '';
-  
-  user = new Observable<GitHubSearch>();
 
-  getUser(username: string){
-    this.user = this.githubService.getData(username)
+  //theme status
+  @Input() darkMode: boolean = false;
+  @Output() darkModeChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  toggleDarkMode(){
+    this.darkMode = !this.darkMode;
+    this.darkModeChange.emit(this.darkMode);
   }
 
-  // processedData(){
-  //   console.log(this.user)
-  // }
+  user$ = new Observable<GitHubSearch>();
+  lastUser: GitHubSearch = {
+    login: '',
+    id: 0,
+    node_id: '',
+    avatar_url: '',
+    gravatar_id: '',
+    url: '',
+    html_url: '',
+    followers_url: '',
+    following_url: '',
+    gists_url: '',
+    starred_url: '',
+    subscriptions_url: '',
+    organizations_url: '',
+    repos_url: '',
+    events_url: '',
+    received_events_url: '',
+    type: '',
+    site_admin: false,
+    name: '',
+    company: '',
+    blog: '',
+    location: '',
+    email: '',
+    hireable: '',
+    bio: '',
+    twitter_username: '',
+    public_repos: 0,
+    public_gists: 0,
+    followers: 0,
+    following: 0,
+    created_at: '',
+    updated_at: ''
+  };
+
+  getUser(username: string){
+    this.user$ = this.githubService.getData(username).pipe(
+      shareReplay(),
+      take(1),
+      startWith(this.lastUser)
+    )
+    this.user$.subscribe(user => {
+    this.lastUser = user});
+
+  }
+
+
+  ngOnInit(): void {
+    this.getUser("octocat");
+    
+  }
+
 }
