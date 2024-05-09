accf4fce7a499179b0043d1d0470f6ee87f52217
Eduardo Moreira <dudu_moreira9@hotmail.com>
2024-05-08 22:50:35 -0300

    create observer and pipes

--- src/app/components/github-search/github-search.component.ts
+++ src/app/components/github-search/github-search.component.ts
@@ -4,11 +4,12 @@ import { GitHubSearchService } from '../../services/git-hub-search.service';
 import { GitHubSearch } from '../../models/search.model';
 import { Observable } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-github-search',
   standalone: true,
-  imports: [HttpClientModule, FormsModule],
+  imports: [HttpClientModule, FormsModule, AsyncPipe],
   providers:[GitHubSearchService],
   templateUrl: './github-search.component.html',
   styleUrl: './github-search.component.scss'
@@ -19,89 +20,14 @@ export class GithubSearchComponent{
   }
   //search response
   searchResponse = '';
-
-  user: GitHubSearch = {
-    login: '',
-    id: 0,
-    node_id: '',
-    avatar_url: '',
-    gravatar_id: '',
-    url: '',
-    html_url: '',
-    followers_url: '',
-    following_url: '',
-    gists_url: '',
-    starred_url: '',
-    subscriptions_url: '',
-    organizations_url: '',
-    repos_url: '',
-    events_url: '',
-    received_events_url: '',
-    type: '',
-    site_admin: false,
-    name: '',
-    company: '',
-    blog: '',
-    location: '',
-    email: '',
-    hireable: '',
-    bio: '',
-    twitter_username: '',
-    public_repos: 0,
-    public_gists: 0,
-    followers: 0,
-    following: 0,
-    created_at: '',
-    updated_at: ''
-  }
-
-  getUser(){
-    this.githubService.getData(this.searchResponse).subscribe(
-      data => {
-        console.log(data)
-        this.user = data;
-      }
-    )
-  }
-
   
+  user = new Observable<GitHubSearch>();
 
+  getUser(username: string){
+    this.user = this.githubService.getData(username)
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // constructor(private http: HttpClient){
-
-  // }
-
-  // data = {}
-
-  // ngOnInit(): void {
-  // }
-
-  // public fetch(){
-  //   this.http.get('https://api.github.com/users/edumoreiira').subscribe(
-  //     (resp: any) => {
-  //       this.data = resp;
-
-  //       console.log(this.data)
-  //     }
-  //   )
+  // processedData(){
+  //   console.log(this.user)
   // }
 }
